da0bcbcbfe13ed98320b01ee009f476b09477923
PLThabangR <rakgoropothabang@gmail.com>
2025-08-26 17:46:40 +0200

    Add user component designed

--- src/components/AddItems/AddItem.tsx
+++ src/components/AddItems/AddItem.tsx
@@ -1,11 +1,11 @@
 import type { Item } from '@/types/ShoppingList';
 import React, { useState } from 'react'
 
-const AddItem = () => {
+const AddItem =async () => {
     const [shoppingList, setShoppingList] = React.useState([]);
 
     const [name,setName] = useState<string>('');
-    const [quantity,setQuantity] = useState<string>('');
+    const [quantity,setQuantity] = useState<number>(0);
     const [notes,setNotes] = useState<string>('');
     const [category,setCategory] = useState<string>('');
     const [imageUrl,setImageUrl] = useState<string>('');
@@ -21,6 +21,31 @@ const AddItem = () => {
             category,
             imageUrl
         }
+
+        //add new item to shopping list
+//Post item to json server
+const response =await fetch('http://localhost:8000/shoppingList', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(newItem),    
+})  
+
+     if(response.ok){
+        const data = await response.json();
+        console.log(data);
+
+        //setShoppingList([...shoppingList,data]);
+        //clear form
+        setName('');
+        setQuantity(0);
+        setNotes('');
+        setCategory('');
+        setImageUrl('');
+  
+
+      
     }
 
   return (
@@ -33,37 +58,32 @@ const AddItem = () => {
     <div className="grid gap-6 mb-6 md:grid-cols-2">
         
         <div>
-            <label htmlFor="first_name" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">First name</label>
-            <input type="text" id="first_name" value={user.firstName} onChange={(e) => setUser({...user, firstName: e.target.value})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+            <label htmlFor="first_name" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Name</label>
+            <input type="text" id="first_name" value={name} onChange={(e) => setName(()=> e.target.value )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bread" required />
         </div>
         <div>
-            <label htmlFor="last_name" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Last name</label>
-            <input type="text" id="last_name" value={user.lastName} onChange={(e) => setUser({...user, lastName: e.target.value})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
+            <label htmlFor="last_name" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Quantity</label>
+            <input type="text" id="last_name" value={quantity} onChange={(e) => setQuantity(()=> Number(e.target.value) )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
         </div>
         
         
     </div>
     <div className="mb-6">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
-        <input type="email" id="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
+        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">description</label>
+        <input type="email" id="email" value={notes} onChange={(e) => setNotes(()=> e.target.value )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
     </div> 
     <div className="mb-6">
-        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-        <input type="password" id="password" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+        <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">category</label>
+        <input type="category" id="category" value={category} onChange={(e) => setCategory(()=> e.target.value )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
     </div> 
 
     <div className="mb-6">
-        <label htmlFor="cellphone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cell number</label>
-        <input type="number" id="cellphone" value={user.cellNumber} onChange={(e) => setUser({...user, cellNumber:Number( e.target.value)})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="082 1234 1234" required />
+        <label htmlFor="imageUrl" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Item image</label>
+        <input type="text" id="imageUrl" value={imageUrl} onChange={(e) => setImageUrl(()=> e.target.value )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="082 1234 1234" required />
     </div> 
-    <div className="flex items-start mb-6">
-        <div className="flex items-center h-5">
-        <input id="remember" type="checkbox" checked={checkBox} onChange={(e) => setCheckBox(e.target.checked)} className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"  />
-        </div>
-        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
-    </div>
-    <button type="submit" className="text-white bg-[#C07858] hover:bg-[#cc927a] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign up</button>
-      <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Already have account <Link to="/login" className="text-blue-600 hover:underline dark:text-blue-500">Login</Link></label>
+    
+    <button type="submit" className="text-white bg-[#C07858] hover:bg-[#cc927a] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add item</button>
+     
 </form>
 
       
