62c65957e1a641b200204c71cd9b1a44999508d4
PLThabangR <rakgoropothabang@gmail.com>
2025-08-28 01:57:51 +0200

    Added modal and app pictures

--- src/components/ItemsCard/ItemCard.tsx
+++ src/components/ItemsCard/ItemCard.tsx
@@ -22,10 +22,73 @@ const ItemCard = (item:Item) => {
  const dispatch = useDispatch();
  //open modal state
  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+     //const {state:LoggedUser} = loggerUser();
+  
+     //form state
+     const [name,setName] = useState<string>('');
+     const [quantity,setQuantity] = useState<number>(0);
+     const [notes,setNotes] = useState<string>('');
+     const [category,setCategory] = useState<string>('');
+     const [imageUrl,setImageUrl] = useState<string>('');
  const openModal = () => {
      setIsModalOpen(true);
  }
+//update item
+const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
+event.preventDefault();
 
+
+ //creat new item
+        const newItem:Item = {
+            name,
+            email:LoggedUser.email,
+            quantity,
+            notes,
+            category,
+            imageUrl
+        }
+        //add new item to state
+      
+
+       
+
+       const response = await fetch('http://localhost:8000/items', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newItem),
+       }) 
+       if(!response.ok){
+        toast.error("Error adding item",{
+            duration:5000,
+            richColors:true
+        })
+       }//end of if
+
+       if(response.ok){
+        
+        const data = await response.json();
+        dispatch(addItems(data));
+        toast.success("Item added successfully",{
+            duration:5000,
+            richColors:true
+
+        })
+
+
+          //clear form
+        setName('');
+        setQuantity(0);
+        setNotes('');
+        setCategory('');
+        setImageUrl('');
+
+       }
+
+}
  const deleteProduct =async (item:Item) => {
      console.log(item)
     //delete item from json server
@@ -74,16 +137,60 @@ const ItemCard = (item:Item) => {
 {/* modal */}
 
 { (<Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
-  
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Update Item</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
+      <DialogContent>
+        <div className="container  w-[100%] m-0 mx-auto">
+
+        
+<form onSubmit={handleSubmit}>
+    <div className="grid gap-6 mb-6 md:grid-cols-2">
+        
+        <div>
+            <label htmlFor="name" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Name</label>
+            <input type="text" id="name" value={item.name} onChange={(e) => setName(()=> e.target.value )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bread" required />
+        </div>
+        <div>
+            <label htmlFor="quantity" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Quantity</label>
+            <input type="text" id="quantity" value={item.quantity} onChange={(e) => setQuantity(()=> Number(e.target.value) )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="10" required />
+        </div>
+        
+        
+    </div>
+    <div className="mb-6">
+        <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+        <input type="text" id="description" value={item.notes} onChange={(e) => setNotes(()=> e.target.value )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="description" required />
+    </div> 
+    <div className="mb-6">
+
+         <label htmlFor="categories" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+  <select id="categories" value={item.category} onChange={(e) => setCategory(()=> e.target.value )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+    <option value="fruits">Fruits</option>
+    <option value={'vegetables'}>Vegetables</option>
+      <option value={'appliances'}>Appliances</option>
+    <option value={'breakfast'}>Breakfast</option>
+    <option value={'dinner'}>Dinner</option>
+    <option value={'beverage'}>Beverage</option>
+    <option value={'dessert'}>Dessert</option>
+    <option value={'snacks'}>Snacks</option>
+     <option value={'frozen'}>Frozen</option>
+      <option value={'fast food'}>fast food</option>
+  </select>
+         
+    </div> 
+
+    <div className="mb-6">
+        <label htmlFor="imageUrl" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image url</label>
+        <input type="text" id="imageUrl" value={item.imageUrl} onChange={(e) => setImageUrl(()=> e.target.value )} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://picsum.photos/200/300" required />
+    </div> 
+    
+    <button type="submit" className="text-white bg-[#C07858] hover:bg-[#cc927a] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-2">Update item </button>
+    
+   
+</form>
+
+    </div>
+     
+      </DialogContent>
 </Dialog>)}
 
 
