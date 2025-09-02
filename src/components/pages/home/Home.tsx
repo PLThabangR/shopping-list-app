c2a092031850a53b17d993c2947ee8c3f5463f67
PLThabangR <rakgoropothabang@gmail.com>
2025-09-02 03:12:55 +0200

    Working on the home ui logic

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -16,8 +16,11 @@ const Home = () => {
 //const {state:items,dispatch:dispatchItems} = useItem();
 const [loading,setLoading] = useState(false);
 
+
 //get logged user
 const loggedUser = useSelector((state:RootState) => state.auth.user)
+//get items search array
+const searchResults = useSelector((state:RootState) => state.items.searchResults);
 const dispatch = useDispatch();
 
 const items = useSelector((state:RootState) => state.items.items);
@@ -66,8 +69,9 @@ const items = useSelector((state:RootState) => state.items.items);
 
    //Display items on card 
    const DisplayItems = items.map((item:Item) => <ItemCard key={item.id} id={item.id} email={item.email} category={item.category} name={item.name} quantity={item.quantity} notes={item.notes} imageUrl={item.imageUrl}  />)
-
-
+   //display item on seacrh array
+   const DisplaySearchedItem= searchResults.map((item:Item) => <ItemCard key={item.id} id={item.id} email={item.email} category={item.category} name={item.name} quantity={item.quantity} notes={item.notes} imageUrl={item.imageUrl}  />)
+  
    useEffect(()=>{
     //run only once when its mounted
 if (items.length === 0) {
@@ -87,10 +91,11 @@ if (items.length === 0) {
     <Navbar/>
         <h1 className="text-2xl shrink-0 font-extrabold text-[#3C3D42] md:text-5xl text-center  m-2 p-2"><span className="text-[#C07858] ">Welcome {loggedUser.firstName}</span>  to your shopping list</h1>
         <p className="text-base font-light shrink-0 text-[#3C3D42] md:text-2xl text-center  m-1 p-1">Let you shopping plan become a success</p>
-          
-        <div className="flex flex-col justify-center items-center  md:flex-row md:justify-evenly md:flex-wrap gap-1 ">
 
-            {items.length > 0 ?DisplayItems: <h1 className="text-2xl shrink-0 font-extrabold text-[#3C3D42] md:text-5xl text-center  m-2 p-2">No items found</h1> }
+          {items.length === 0 ?<h1 className="text-2xl shrink-0 font-extrabold text-[#3C3D42] md:text-5xl text-center  m-2 p-2">No items found</h1>:<h1 className="text-2xl shrink-0 font-extrabold text-[#3C3D42] md:text-5xl text-center  m-2 p-2">Avaliable items</h1>}
+        <div className="flex flex-col justify-center items-center  md:flex-row md:justify-evenly md:flex-wrap gap-1 ">
+             
+            {searchResults.length > 0 ?DisplaySearchedItem:DisplayItems  }
           
         </div>
     </div>
